102f50787a82c60e291705df3fef2049599a563f
Margar18 <61972481+Margar18@users.noreply.github.com>
2022-02-09 00:26:36 +0530

    Add files via upload

---
+++ practise.py
@@ -0,0 +1,58 @@
+"""""
+Testing of multiple line comments
+using pycharm
+"""""
+
+
+#first code
+print("Hello World")
+
+
+#varibale examples
+age=45
+sal=123.456
+name="John"
+print(name)
+print(age)
+print(sal)
+
+#operators example
+a=4
+b=5.5
+
+#addition
+sum=a+b
+print(sum)
+
+#subtration
+dif=b-a
+print(dif)
+dif2=a-b
+print(dif2)
+
+#multiplication
+mul=a*b
+print(mul)
+
+#division
+div=a/b
+div2=b/a
+print(div)
+print(div2)
+
+
+#relational operator
+a1=5
+a2=10
+print(a1!=a2)
+print(a1>a2)
+print(a1<a2)
+print(a1==a2)
+print(a1>=a2)
+print(a1<=a2)
+
+#to find type
+A='I am margaret'
+print("'I am margaret' is a : ", type(A))
+c= 2+4j
+print("'2+4j' is a: ", type(c))
